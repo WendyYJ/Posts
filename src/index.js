357b298e1fdeb5fc47b42cd12f8f9820294d10c3
Wendy Yu <y_jies@hotmail.com>
2020-01-14 13:43:33 +1000

    update port output format

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
 const app = express();
 const  routes = require("./routes");
 require('dotenv').config();
 const bodyPaser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 
+app.use(bodyPaser.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));// dev for development evironment. common for writing codes
-app.use(bodyPaser.json());
 app.use('/v1',routes);
 
-app.listen(PORT,() => {
-    console.log("listening on 3000");
+app.listen(process.env.port,() => {
+    console.log(`listening on ${PORT}`);
 });
